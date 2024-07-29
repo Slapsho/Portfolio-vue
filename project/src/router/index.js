dcde86fdbc2e5e32930ef0979c7029b74bbc8c58
slapsho <sanniervictor@gmail.com>
2024-07-29 10:35:01 +0200

    modification du router pour la nouvelle page

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+import Work from '../views/Work.vue';
 import NotFound from '../views/NotFound.vue';
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/work', component: Work },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }
 ];
 
