7322ea8da15d64abc09acab2f82ab26de4022108
slapsho <sanniervictor@gmail.com>
2024-07-29 10:02:07 +0200

    erreur 404

---
+++ project/src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="not-found">
+      <img src="@/assets/404.png" alt="404" />
+      <h1>404</h1>
+      <p>Page non trouvée</p>
+    </div>
+  </template>
+  
+  <style scoped>
+  .not-found {
+    text-align: center;
+    margin-top: 50px;
+  }
+  </style>
+  
