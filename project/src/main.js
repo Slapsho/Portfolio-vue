6b3c6f1f37aa4e46b90f53829d71c055d28d4609
slapsho <sanniervictor@gmail.com>
2024-07-29 09:38:40 +0200

    modification src/main.js

--- project/src/main.js
+++ project/src/main.js
@@ -2,3 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import './styles/main.css';
+
+createApp(App).use(router).mount('#app');
