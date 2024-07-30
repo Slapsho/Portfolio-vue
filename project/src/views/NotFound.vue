a6d43cab9f1203cf8befe56ce331b3d1b7480871
slapsho <sanniervictor@gmail.com>
2024-07-30 14:29:28 +0200

    ajustement 404

--- project/src/views/NotFound.vue
+++ project/src/views/NotFound.vue
@@ -1,15 +1,25 @@
 <template>
     <div class="not-found">
-      <img src="@/assets/404.png" alt="404" />
+      <img src="../assets/image/Error404.jpg" alt="404" />
       <h1>404</h1>
       <p>Page non trouvée</p>
     </div>
   </template>
   
+  <script>
+  export default {
+    name: 'NotFound'
+  };
+  </script>
+  
   <style scoped>
   .not-found {
     text-align: center;
     margin-top: 50px;
+  }
+  .not-found img {
+    max-width: 100%;
+    height: auto;
   }
   </style>
   
